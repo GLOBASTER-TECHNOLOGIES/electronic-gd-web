0ed7b1477c34c864525e5b840ab3e76a72f679ec
Noel Sebu <noelsebu555@gmail.com>
2026-02-11 18:40:15 +0530

    field name updated

--- app/api/admin/post/get-post-data/route.ts
+++ app/api/admin/post/get-post-data/route.ts
@@ -31,16 +31,15 @@ export async function GET(request: NextRequest) {
 
     // 3. Fetch from DB
     const posts = await Post.find(query)
-      .populate("officerInCharge", "officerName rank forceNumber")
+      .populate("officerInCharge", "name rank forceNumber")
       .sort({ division: 1, postName: 1 });
-
+    console.log(posts);
     return NextResponse.json({ success: true, data: posts }, { status: 200 });
-
   } catch (error) {
     console.error("Filter Posts Error:", error);
     return NextResponse.json(
-      { success: false, message: "Internal Server Error" }, 
-      { status: 500 }
+      { success: false, message: "Internal Server Error" },
+      { status: 500 },
     );
   }
 }
